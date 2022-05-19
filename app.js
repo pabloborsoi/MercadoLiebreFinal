1df10784b04033f5e67fea7c47736f1d176f7c18
pabloborsoi <pabloborsoi@users.noreply.github.com>
2022-05-18 22:08:42 -0300

    configuracion de app.js

--- app.js
+++ app.js
@@ -1,24 +1,8 @@
-const http = require('http');
-const fs = require('fs');
-const port = 3000;
-const server = http.createServer(function(req,res){
-    res.writeHead(200,{'Content-Type':'text/html'})
-    fs.readFile('index.html',function(error,data){
-        if(error){
-            res.writeHead(404)
-            res.write('Error: File Not Found')
-        }
-        else{
-            res.write(data)
-        }
-    })
-})
+const express= require("express");
+const app = express();
+app.use(express.static('public'));
+app.listen("3000",()=>console.log("Servidor Corriendo"));
+app.get("/",(req,res)=>res.sendFile(__dirname + "/views/home.html"));
+app.get("/register",(req,res)=>res.sendFile(__dirname + "/views/register.html"));
+app.get("/login",(req,res)=>res.sendFile(__dirname + "/views/login.html"));
 
-server.listen(port,function(error){
-    if(error){
-        console.log('Algo anduvo mal',error)
-    }
-    else{
-        console.log('El servidor esta corriendo en el puerto '+port)
-    }
-})
